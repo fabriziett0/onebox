9741851fc5b42b11ac084eb8473255789e6c4352
Fabrizio <fabrizio@multiconnect.it>
2024-06-28 18:28:35 +0200

    .

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
